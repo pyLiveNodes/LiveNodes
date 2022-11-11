f04ecfa2e44a0f09441c74b03580d52f2c98a7f1
yhartmann <yale.hartmann@uni-bremen.de>
2022-11-11 09:14:41 +0100

    fixed log drainer threads not terminating

--- src/livenodes/components/utils/log.py
+++ src/livenodes/components/utils/log.py
@@ -1,3 +1,4 @@
+import queue
 import logging
 import traceback
 import sys
@@ -8,8 +9,10 @@ def drain_log_queue(parent_log_queue, logger_name, stop_log_event):
     logger = logging.getLogger(logger_name)
     while not stop_log_event.is_set():
         try:
-            record = parent_log_queue.get()
+            record = parent_log_queue.get(timeout=0.1)
             logger.handle(record)
+        except queue.Empty:
+            pass
         except (KeyboardInterrupt, SystemExit):
             raise
         except EOFError:
