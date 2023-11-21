d464d5442f0da0454535b518c29d4762bad304b7
Yale Hartmann <yale.hartmann@uni-bremen.de>
2023-11-21 10:12:56 +0100

    established tests

--- src/livenodes/components/port.py
+++ src/livenodes/components/port.py
@@ -38,9 +38,9 @@ class Port():
             and self.key == other.key
 
     def __init_subclass__(cls):
-        if cls.compound_type is not None:
-            compunded_examples = list(filter(lambda x: cls.check_value(x)[0], map(cls.example_compound_construction, cls.compound_type.example_values)))
-            cls.example_values += compunded_examples
+        # if cls.compound_type is not None:
+        #     compunded_examples = list(filter(lambda x: cls.check_value(x)[0], map(cls.example_compound_construction, cls.compound_type.example_values)))
+        #     cls.example_values += compunded_examples
 
         if len(cls.example_values) <= 0:
             raise Exception('Need to provide at least one example value.')
---
+++ tests/port_test.py
@@ -0,0 +1,106 @@
+import pytest
+
+from livenodes.components.port import Port, ALL_VALUES
+import numpy as np
+
+# === Special Case Any ========================================================
+class Port_Any(Port):
+    # TODO: figure out how to automatically extend this with each new primitive (?) port class added...
+    example_values = ALL_VALUES
+
+    @classmethod
+    def check_value(cls, value):
+        return True, None
+    
+
+class Port_Int(Port):
+    example_values = [
+        0,
+        1,
+        np.array([1])[0]
+    ]
+
+    @classmethod
+    def check_value(cls, value):
+        if not isinstance(value, int):
+            try:
+                if np.issubdtype(value, np.integer):
+                    return True, None
+                else:
+                    return False, f"Should be int; got {type(value)}, val: {value}."
+            except:
+                return False, f"Should be int; got {type(value)}, val: {value}."
+        return True, None
+
+# === Compounds ========================================================
+
+# TODO: consider if it really makes sense to mix lists and arrays...
+class Port_List(Port):
+    example_values = [
+        ["EMG1", "EMG2", "EMG2"],
+        [0, 1],
+        [0, -1],
+        [20, .1, .1],
+        np.array([20, 1])
+    ]
+
+    compound_type = Port_Any
+
+    @classmethod
+    def example_compound_construction(cls, compounding_value):
+        return [compounding_value]
+
+    @classmethod
+    def check_value(cls, value):
+        if not (type(value) == list or isinstance(value, np.ndarray)):
+            return False, f"Should be list; got {type(value)}, val: {value}."
+        if len(value) > 0:
+            return cls.compound_type.check_value(value[-1])
+        return True, None
+
+class Port_List_Int(Port_List):
+    example_values = [
+        [0, 1]
+    ]
+    compound_type = Port_Int
+    
+
+class TestPorts():
+
+    def test_any_value(self):
+        a = Port_Any("")
+        assert a.check_value(1)[0]
+        assert a.check_value("None")[0]
+        assert a.check_value(None)[0]
+        assert a.check_value([])[0]
+        assert a.check_value([{'a': [5]}])[0]
+
+    def test_int_value(self):
+        a = Port_Int("")
+        assert a.check_value(1)[0]
+        assert a.check_value(-200)[0]
+        assert not a.check_value(None)[0]
+        assert not a.check_value([])[0]
+        assert not a.check_value([1])[0]
+        assert not a.check_value([{'a': [5]}])[0]
+
+    def test_list_value(self):
+        a = Port_List("")
+        assert a.check_value([])[0]
+        assert a.check_value([[1]])[0]
+        assert a.check_value([[1, -3]])[0]
+        assert a.check_value([['1', -3]])[0]
+        assert not a.check_value(None)[0]
+
+    def test_list_int_value(self):
+        a = Port_List_Int("")
+        assert a.check_value([])[0]
+        assert a.check_value([1, -1, 2])[0]
+        assert not a.check_value([[1]])[0]
+        assert not a.check_value([[1, -3]])[0]
+        assert not a.check_value([['1', -3]])[0]
+        assert not a.check_value(None)[0]
+
+
+        
+
