fc038a9f873e08516cd4fedd05981dc94095b06f
Yale Hartmann <yale.hartmann@uni-bremen.de>
2024-10-29 12:14:42 +0100

    lets hope this fixes the publish error

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown'
+    )
     
