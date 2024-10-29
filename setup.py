ad881439626eb2c6d8457828ff53e4f7464187e3
Yale Hartmann <yale.hartmann@uni-bremen.de>
2024-10-29 12:19:52 +0100

    fingers crossed

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        long_description=open('README.md').read(),
-        long_description_content_type='text/markdown'
-    )
+    setup()
     
