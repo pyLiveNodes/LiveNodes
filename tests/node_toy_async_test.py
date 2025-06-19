559ade611ca1c8970d02cf6292aad2d8ccb1c0cc
Yale Hartmann <yale.hartmann@uni-bremen.de>
2025-06-19 14:13:11 +0200

    some test cleanup

--- tests/node_toy_async_test.py
+++ tests/node_toy_async_test.py
@@ -85,46 +85,24 @@ class TestProcessingAsync():
         assert out2.get_state_and_close() == list(map(lambda x: x**2, range(10)))
         assert g.is_finished()
 
-    def test_calc_fail(self, create_simple_graph_fail):
-        def test_calc_fail(self, create_simple_graph_fail, caplog):
-            data, quadratic, out1, out2 = create_simple_graph_fail
+    def test_calc_fail(self, create_simple_graph_fail, caplog):
+        data, quadratic, out1, out2 = create_simple_graph_fail
 
-            # capture errors
-            caplog.set_level(logging.ERROR)
-
-            g = Graph(start_node=data)
-            g.start_all()
-            g.join_all()
-            g.stop_all()
-
-            # only values before the error should be pushed
-            assert out1.get_state_and_close() == list(range(6))
-            assert out2.get_state_and_close() == list(map(lambda x: x**2, range(6)))
-
-            # check that the ValueError was logged
-            assert "ValueError: Test error" in caplog.text
-
-            assert g.is_finished()
-
-    def test_calc_twice(self, create_simple_graph):
-        data, quadratic, out1, out2 = create_simple_graph
+        # capture errors
+        caplog.set_level(logging.ERROR)
 
         g = Graph(start_node=data)
         g.start_all()
         g.join_all()
         g.stop_all()
 
-        assert out1.get_state_and_close() == list(range(10))
-        assert out2.get_state_and_close() == list(map(lambda x: x**2, range(10)))
-        assert g.is_finished()
+        # only values before the error should be pushed
+        assert out1.get_state_and_close() == list(range(6))
+        assert out2.get_state_and_close() == list(map(lambda x: x**2, range(6)))
 
-        g = Graph(start_node=data)
-        g.start_all()
-        g.join_all()
-        g.stop_all()
+        # check that the ValueError was logged
+        assert "ValueError: Test error" in caplog.text
 
-        assert out1.get_state_and_close() == list(range(10))
-        assert out2.get_state_and_close() == list(map(lambda x: x**2, range(10)))
         assert g.is_finished()
 
     def test_calc_twice(self, create_simple_graph):
@@ -135,15 +113,23 @@ class TestProcessingAsync():
         g.join_all()
         g.stop_all()
 
+        outdeps = data.discover_output_deps(data)
+        out1 = [o for o in outdeps if o.name == 'C'][0]
+        out2 = [o for o in outdeps if o.name == 'D'][0]
         assert out1.get_state_and_close() == list(range(10))
         assert out2.get_state_and_close() == list(map(lambda x: x**2, range(10)))
         assert g.is_finished()
 
+        # run again but not with a copy. i don't want to support running the same graph twice as that would require us to rewrite all the states
+        data = data.copy(graph=True)
         g = Graph(start_node=data)
         g.start_all()
         g.join_all()
         g.stop_all()
 
+        outdeps = data.discover_output_deps(data)
+        out1 = [o for o in outdeps if o.name == 'C'][0]
+        out2 = [o for o in outdeps if o.name == 'D'][0]
         assert out1.get_state_and_close() == list(range(10))
         assert out2.get_state_and_close() == list(map(lambda x: x**2, range(10)))
         assert g.is_finished()
